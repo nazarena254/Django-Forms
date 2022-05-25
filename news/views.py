772fc23f2b9b2b7e0b1c78c10699b11c43e9a610
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-25 10:38:27 +0300

    edit functions

--- news/views.py
+++ news/views.py
@@ -54,6 +54,6 @@ def search_results(request):
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
-    except DoesNotExist:
+    except Article.DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
