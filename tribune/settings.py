0c8e5dab5fde26d569e2e3ba749526eb5d3657b2
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-26 11:25:30 +0300

    deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -27,32 +27,9 @@ SECRET_KEY = 'django-insecure-$2y@+vqe5xl+h911@b^h7-4)fo&pzpg+2bc-ai!d1as)l&!ly4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-# development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
-       
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
+
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
