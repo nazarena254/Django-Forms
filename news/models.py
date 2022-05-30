5d6823edf8810529799740dbd2101d129ee37357
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-30 11:43:54 +0300

    add view functions

--- news/models.py
+++ news/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
+
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
