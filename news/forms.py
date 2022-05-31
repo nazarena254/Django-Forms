015768ae5b5a24249f81dbb5505e443498d4c197
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-31 17:32:56 +0300

    install python-decouple so as to connect the environment variables to our Django

--- news/forms.py
+++ news/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     last_name = forms.CharField(label='Last Name',max_length=40)
