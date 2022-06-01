2271b32b99d714296a81435b2c670def65895a2b
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-01 23:46:45 +0300

    update new/article url

--- news/urls.py
+++ news/urls.py
@@ -7,6 +7,6 @@ urlpatterns=[
     path('',views.news_today,name='newsToday'),
     path('archives/',views.past_days_news,name = 'pastNews'),
     path('search/', views.search_results, name='search_results'),
-    path('article/',views.article,name ='article')   
+    path('new/article/',views.new_article,name ='new_article')   
      
 ]
