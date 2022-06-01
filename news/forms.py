aac64666ccdd7434127c2b57cd59270c9ba51143
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-01 23:23:14 +0300

    create form for the new article

--- news/forms.py
+++ news/forms.py
@@ -1,7 +1,16 @@
+from tkinter import Widget
 from django import forms
-
+from .models import Article
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     your_lastname = forms.CharField(label='Last Name',max_length=40)
     email = forms.EmailField(label='Email')
+
+class NewArticleForm(forms.modelForm):
+    class Meta:
+        model=Article
+        exclude=['editor','pub_date']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
