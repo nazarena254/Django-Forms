2b5fe47b5dfb7a34b48623752cfc082c03e4b16c
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-01 22:54:59 +0300

    add users to be editors

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -27,7 +28,7 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
     article_image = models.ImageField(upload_to = 'articles/',blank=True)
