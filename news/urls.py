adf8bc83534b62272f46a4e52ed77fe1778fe2a3
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-07 14:57:55 +0300

    add a url path for ajax request page

--- news/urls.py
+++ news/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     path('',views.news_today,name='newsToday'),
     path('archives/',views.past_days_news,name = 'pastNews'),
     path('search/', views.search_results, name='search_results'),
-    path('new/article/',views.new_article,name ='new-article')   
+    path('new/article/',views.new_article,name ='new-article'), 
+    path('ajax/', views.newsletter, name='newsletter'),  
      
 ]
