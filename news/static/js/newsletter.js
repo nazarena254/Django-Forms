c2fef18c7b4c29866449dd65db91895788f1cd8a
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-07 15:01:33 +0300

    create ajax fn in js file

--- news/static/js/newsletter.js
+++ news/static/js/newsletter.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
     $('form').submit(function(event){
         event.preventDefault()
+        form = $('form')
+
+        $.ajax({
+            'url':'/ajax/newsletter/',
+            'type':'POST',
+            'data':form.serialize(),
+            'dataType':'json',
+            'success': function(data){
+              alert(data['success'])
+            },
+          })// END of Ajax method
+          // used to get values for user details
+          $('#id_your_name').val('')
+          $('#id_your_lastname').val('')
+          $("#id_email").val('')
+
     }) // End of submit event
-    
+
 }) // End of document ready function
