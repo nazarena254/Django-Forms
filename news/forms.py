f787c06f4a327b06e7dab1eff0c1cfad55979a55
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-07 14:16:53 +0300

    add from django.forms import ModelForm in my forms.py import

--- news/forms.py
+++ news/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django import forms
 from .models import Article
 
+
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     your_lastname = forms.CharField(label='Last Name',max_length=40)
